89be98ab7aab21241c0c42c8a440dddaf0007a87
AsmahanAL-yemeni <143809470+AsmahanAL-yemeni@users.noreply.github.com>
2023-09-01 16:06:15 +0300

    Add files via upload

---
+++ contacts.js
@@ -0,0 +1,15 @@
+[
+{"name":"asma","phone":"777888000","email":"asma@gmil.com","adderss":"sanaa"},
+
+{"name":"asmahan","phone":"777881100","email":"asmahan@gmil.com","adderss":"sanaa"},
+
+{"name":"muhammed","Phone":"777558000","email":"mustafa@gmil.com","adderss":"sanaa"},
+
+{"name":"ali","Phone":"777968000","email":"ali@gmil.com","adderss":"sanaa"},
+
+{"name":"noor","phone":"777228000","email":"noor@gmil.com","adderss":"sanaa"}
+
+
+
+
+]
